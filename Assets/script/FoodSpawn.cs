ceff455dcc1432348f9b4cf16cbbbe072e7a7b02
MClar-bit <clar.miu@gmail.com>
2025-05-23 09:24:27 +0900

    Fixed food Spawn

--- Assets/script/FoodSpawn.cs
+++ Assets/script/FoodSpawn.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class FoodSpawn : MonoBehaviour
 {
     public GameObject food;
-    public float offset = 10;
+    public float offset = 2;
 
-    public void spawnFood() {
+    public void spawnFood()
+    {
+        float lowestPoint = transform.position.x - offset;
+        float highestPoint = transform.position.x + offset;
 
-    Instantiate(food, new Vector3(transform.position.x, transform.position.y,  0), transform.rotation);
+        Instantiate(food, new Vector3(Random.Range(lowestPoint, highestPoint), transform.position.y, 0), transform.rotation);
     }
+    
+    
 }
